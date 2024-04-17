0e198f6e70d2b15e96d961b79fb5da1e8c05bf95
cjans121 <jpanthi@utexas.edu>
2024-04-17 15:06:22 -0400

    add wbc trajectory msg converter

--- spot_manipulation_driver/spot_manipulation_driver/ros_helpers.py
+++ spot_manipulation_driver/spot_manipulation_driver/ros_helpers.py
@@ -11,6 +11,47 @@ import rclpy.time
 from .spot_manipulation_driver import SpotManipulationDriver
 
 
+def wbc_joint_trajectory_to_lists(msg: JointTrajectory):
+    traj_point_positions = []
+    traj_point_velocities = []
+    timepoints = []
+
+    # Order of joints
+    joint_order = [
+        "body_x",
+        "body_y",
+        "body_or",
+        "arm0_shoulder_yaw",
+        "arm0_shoulder_pitch",
+        "arm0_elbow_pitch",
+        "arm0_elbow_roll",
+        "arm0_wrist_pitch",
+        "arm0_wrist_roll",
+    ]
+
+    # Reorder joint commands based on joint_order and put them into long lists of lists
+    point: JointTrajectoryPoint
+    for point in msg.points:
+        pos_dict = {}
+        # vel_dict = {}
+
+        for j in range(0, 6):
+            name = msg.joint_names[j]
+            pos_dict[name] = point.positions[j]
+            # vel_dict[name] = point.velocities[j]
+
+        traj_point_positions.append(
+            list(map(lambda joint_name: pos_dict[joint_name], joint_order))
+        )
+        # traj_point_velocities.append(
+        #     list(map(lambda joint_name: vel_dict[joint_name], joint_order))
+        # )
+        timepoints.append(
+            point.time_from_start.sec + point.time_from_start.nanosec * 1e-9
+        )
+
+    return traj_point_positions, traj_point_velocities, timepoints
+
 def joint_trajectory_to_lists(msg: JointTrajectory):
     traj_point_positions = []
     traj_point_velocities = []
