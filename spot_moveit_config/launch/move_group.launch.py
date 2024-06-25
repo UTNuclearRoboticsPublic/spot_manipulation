e8368ae62bed87a6ebfc4eca2bf16bad7bfd633a
alexnavtt <alexnavtt@utexas.edu>
2024-06-24 18:03:01 -0600

    Updated move group launch to use correct joint states

--- spot_moveit_config/launch/move_group.launch.py
+++ spot_moveit_config/launch/move_group.launch.py
@@ -1,8 +1,17 @@
+from launch import LaunchDescription
+from launch.actions import GroupAction
+from launch_ros.actions import SetRemap
 from moveit_configs_utils import MoveItConfigsBuilder
 from moveit_configs_utils.launches import generate_move_group_launch
-
 
 def generate_launch_description():
     moveit_config = MoveItConfigsBuilder("spot", package_name="spot_moveit_config").to_moveit_configs()
     moveit_config.move_group_capabilities["capabilities"] = ""
-    return generate_move_group_launch(moveit_config)
+    return LaunchDescription([
+        GroupAction(
+            actions=[
+                SetRemap(src='/joint_states', dst='/spot_driver/joint_states'),
+                generate_move_group_launch(moveit_config)
+            ]
+        )
+    ])
