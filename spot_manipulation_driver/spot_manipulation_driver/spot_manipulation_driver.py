b440125c3eb72ff5bafb1553ba4e9be0528c8251
alexnavtt <alexnavtt@utexas.edu>
2024-07-31 14:54:08 -0600

    Implemented a maximum velocity for the arm

--- spot_manipulation_driver/spot_manipulation_driver/spot_manipulation_driver.py
+++ spot_manipulation_driver/spot_manipulation_driver/spot_manipulation_driver.py
@@ -229,8 +229,7 @@ class SpotManipulationDriver(object):
                 joint_positions=window,
                 times=timestamps,
                 ref_time=ref_time,
-                max_acc=10000,
-                max_vel=10000,
+                max_vel=2.0
             )
 
             success, msg, _ = self._lease_manager.robot_command(robot_cmd)
