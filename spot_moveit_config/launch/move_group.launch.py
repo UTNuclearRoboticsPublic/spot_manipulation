295c17a23dc5bfabf1a7488c8079b6a3c1c1f456
alexnavtt <alexnavtt@utexas.edu>
2024-09-06 18:19:59 -0500

    Added necessary files for the cuMotion plugin to load in Rviz

--- spot_moveit_config/launch/move_group.launch.py
+++ spot_moveit_config/launch/move_group.launch.py
@@ -5,7 +5,9 @@ from moveit_configs_utils import MoveItConfigsBuilder
 from moveit_configs_utils.launches import generate_move_group_launch
 
 def generate_launch_description():
-    moveit_config = MoveItConfigsBuilder("spot", package_name="spot_moveit_config").to_moveit_configs()
+    moveit_config_builder = MoveItConfigsBuilder("spot", package_name="spot_moveit_config")
+    moveit_config_builder.planning_pipelines("ompl", ["ompl", "isaac_ros_cumotion"])
+    moveit_config = moveit_config_builder.to_moveit_configs()
     moveit_config.move_group_capabilities["capabilities"] = ""
     return LaunchDescription([
         GroupAction(
