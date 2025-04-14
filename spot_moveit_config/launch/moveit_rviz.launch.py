a429fea9d97ba6a6a4fd9db48c5da87df585ca6e
alexnavtt <alexnavtt@utexas.edu>
2025-04-14 14:12:13 -0600

    Changed naming and started process to create importable function

--- spot_moveit_config/launch/moveit_rviz.launch.py
+++ spot_moveit_config/launch/moveit_rviz.launch.py
@@ -3,14 +3,14 @@ from moveit_configs_utils.launches import generate_moveit_rviz_launch
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument
 from launch.substitutions import LaunchConfiguration
-from launch_ros.actions import SetRemap, Node
+from launch_ros.actions import SetRemap, Node, PushRosNamespace
 from spot_description.get_accessories import get_accessories_from_env
 
 def generate_launch_description():
-    spot_accessories = get_accessories_from_env()
+    xacro_args = get_accessories_from_env()
     moveit_config_builder = MoveItConfigsBuilder("spot", package_name="spot_moveit_config")
-    moveit_config_builder.robot_description(mappings=spot_accessories)
-    moveit_config_builder.robot_description_semantic(mappings=spot_accessories)
+    moveit_config_builder.robot_description(mappings=xacro_args)
+    moveit_config_builder.robot_description_semantic(mappings=xacro_args)
     
     moveit_config = moveit_config_builder.to_moveit_configs()
 
@@ -35,9 +35,11 @@ def generate_launch_description():
     )
 
     return LaunchDescription([
+        # PushRosNamespace("spot_moveit"),
         SetRemap(src='/spot_moveit/joint_states', dst='/spot_driver/joint_states'),
         SetRemap(src='/spot_moveit/arm_controller/follow_joint_trajectory', dst='/arm_controller/follow_joint_trajectory'),
         SetRemap(src='/spot_moveit/body_manipulation_controller/follow_joint_trajectory', dst='/body_manipulation_controller/follow_joint_trajectory'),
+        SetRemap(src='/planning_scene', dst='/spot_moveit/planning_scene'),
         rviz_config,
         rviz_node
     ])
